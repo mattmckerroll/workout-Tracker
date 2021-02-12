1e458fe6cafc5a07f6ee9d2a5a614c1ffd243c4d
mattmck <matthew.mckerroll@gmail.com>
2021-02-11 23:07:45 -0500

    attempting to fix heroku issue

--- models/workout.js
+++ models/workout.js
@@ -43,6 +43,6 @@ const WorkoutSchema = new Schema({
 
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("workout", WorkoutSchema);
 
 module.exports = Workout;
