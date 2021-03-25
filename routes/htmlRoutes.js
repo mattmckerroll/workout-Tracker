40b904fcbbd8eb1d47f47afc7d59d53fd1702540
Matthew Mckerroll <matthew.mckerroll@gmail.com>
2021-03-24 21:12:34 -0400

    fixed html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,17 @@
-const db = require('../models')
-const path = require('path');
+  
+const router = require("express").Router();
+const path = require("path");
 
-module.exports = (app) => {
-
-    //main page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-//exercise page
-app.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-//stats page 
-app.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/stats.html'));
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
-}
+
+module.exports = router;
